168132e386e3f3b7b72ac2c65278fa7d4d6b215d
naTpuk-ep <naTpuk.ep@gmail.com>
2020-11-06 20:55:11 +0300

    feat: request

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import ReactAutocomplete from 'react-autocomplete'
+import axios from 'axios'
 
 function App() {
-  const [value, setValue] = useState();
+  const [value, setValue] = useState('');
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    axios.get(`https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=${value}`)
+      .then(function (response) {
+        const items = [];
+        let data = response.data[1];
+        if (Array.isArray(data)) {
+          response.data[1].forEach((item, i) => {
+            items.push({
+              id: response.data[3][i], 
+              label: item
+            })
+          })
+        }
+        setItems(items);
+        // console.log(items);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }, [value])
+
+
   return (
     <ReactAutocomplete
-      items={[
-        { id: 'foo', label: 'foo' },
-        { id: 'bar', label: 'bar' },
-        { id: 'baz', label: 'baz' },
-      ]}
+      items={items}
       shouldItemRender={(item, value) => item.label.toLowerCase().indexOf(value.toLowerCase()) > -1}
       getItemValue={item => item.label}
       renderItem={(item, highlighted) =>
